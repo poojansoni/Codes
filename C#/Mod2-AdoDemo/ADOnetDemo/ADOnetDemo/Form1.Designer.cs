46677a3c29f88b057b95391e8af7100c4704e2e1
Poojan <poojansoni.udr@email.com>
2021-03-17 13:03:25 +0530

    Connecting sql server to code

---
+++ C#/Mod2-AdoDemo/ADOnetDemo/ADOnetDemo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ADOnetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetByDataReader = new System.Windows.Forms.Button();
+            this.DataAdapter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GetByDataReader
+            // 
+            this.GetByDataReader.Location = new System.Drawing.Point(367, 80);
+            this.GetByDataReader.Name = "GetByDataReader";
+            this.GetByDataReader.Size = new System.Drawing.Size(199, 71);
+            this.GetByDataReader.TabIndex = 0;
+            this.GetByDataReader.Text = "DataReader";
+            this.GetByDataReader.UseVisualStyleBackColor = true;
+            this.GetByDataReader.Click += new System.EventHandler(this.GetByDataReader_Click);
+            // 
+            // DataAdapter
+            // 
+            this.DataAdapter.Location = new System.Drawing.Point(69, 80);
+            this.DataAdapter.Name = "DataAdapter";
+            this.DataAdapter.Size = new System.Drawing.Size(199, 71);
+            this.DataAdapter.TabIndex = 1;
+            this.DataAdapter.Text = "DataAdapter";
+            this.DataAdapter.UseVisualStyleBackColor = true;
+            this.DataAdapter.Click += new System.EventHandler(this.DataAdapter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 228);
+            this.Controls.Add(this.DataAdapter);
+            this.Controls.Add(this.GetByDataReader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetByDataReader;
+        private System.Windows.Forms.Button DataAdapter;
+    }
+}
+
---
+++ C#/Mod2-AdoDemo/ADOnetDemo/ADOnetDemo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADOnetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetByDataReader_Click(object sender, EventArgs e)
+        {
+            ConnectByDataReader();
+        }
+
+        private void ConnectByDataReader()
+        {
+
+            //1> Setup connection string:
+            // 1.Data Server(sql) = Data Source //"Server Name"
+            //2. DataBase Name = Initial Catalog=Northwind
+            //3. UserName      = Sa user
+            //4. Password      = sa Password
+            //5. Security Mode = Integrated Security=True             
+
+            string connectionString = @"Data Source=DESKTOP-0LC4GSM;Initial Catalog=Northwind;Integrated Security=True";
+
+            //Step 2 : Create SqlConnection object and assign connection string
+            using(SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                //Step 3 : create a SqlCommand Object
+                string cmd = "SELECT FirstName, City FROM Employees where City ='Seattle'";
+
+                using (SqlCommand command = new SqlCommand(cmd,sqlConnection))
+                {
+
+                    //Step 4: Set Command Type
+                    command.CommandType = CommandType.Text;
+
+                    sqlConnection.Open();
+
+                    //Step 5 : 2 options - 1. DataReader , 2. Data Adapter
+                    SqlDataReader dataReader = command.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        string name = dataReader["FirstName"].ToString();
+
+                        string city = dataReader["City"].ToString();
+
+                        MessageBox.Show("Name: "+name+ " City: " + city);
+                    }
+
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private void DataAdapter_Click(object sender, EventArgs e)
+        {
+            GetByDataAdapter();
+        }
+
+        private void GetByDataAdapter()
+        {
+            string cnct = @"Data Source=DESKTOP-0LC4GSM;Initial Catalog=Northwind;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(cnct))
+            {
+                string cmd = "Select FirstName,City from Employees where City='Seattle'";
+                using (SqlCommand command = new SqlCommand(cmd,connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+
+                    // Step 5 : 2 options - 1.DataReader , 2.Data Adapter
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter(command))
+                    {
+                        DataSet dataSet = new DataSet();
+                        dataAdapter.Fill(dataSet);
+                        connection.Close();
+                        foreach (DataRow row in dataSet.Tables[0].Rows)
+                        {
+                            string name = row["FirstName"].ToString();
+
+                            string city = row["City"].ToString();
+
+                            MessageBox.Show("Name: " + name + " City: " + city);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
